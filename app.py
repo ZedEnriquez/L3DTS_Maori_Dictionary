13ff89aa321f389e3db01e1d74ecd5665c247c41
Zed Enriquez <18118@wc.school.nz>
2022-05-03 19:17:08 +1200

    3/5/2022

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# Zed's Maori Dictionary
+
+# Imports
 from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
@@ -8,11 +11,15 @@ from smtplib import SMTPAuthenticationError
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+# Necessary Code
 app = Flask(__name__)
-bcrypt = Bcrypt(app)     # Bcrypt is used to hash the user's passwords.
 DATABASE = 'maori_dictionary.db'
 app.secret_key = "twentyone"
 
+# Bcrypt is used to hash the user's passwords.
+bcrypt = Bcrypt(app)
+
+# Collecting Category Names
 def categories():
     query = "SELECT category_name FROM categories"
     con = create_connection(DATABASE)
@@ -22,7 +29,8 @@ def categories():
     con.close()
     return category_list
 
-def create_connection(db_file):     # Connects the desired database file
+# Connecting the database
+def create_connection(db_file):
     try:
         connection = sqlite3.connect(db_file)
         connection.execute('pragma foreign_keys=ON')
@@ -31,6 +39,7 @@ def create_connection(db_file):     # Connects the desired database file
         print(e)
     return None
 
+# Checking the login state
 def is_logged_in():
     if session.get("email") is None:
         print("not logged in")
@@ -39,66 +48,12 @@ def is_logged_in():
         print("logged in")
         return True
 
+# Homepage
 @app.route('/')
 def render_homepage():
     return render_template("home.html", logged_in=is_logged_in(), categories=categories()) #, categories_exist=category_list_exists)
 
-@app.route('/login', methods=["GET", "POST"])
-def render_login_page():
-    if is_logged_in():
-        return redirect('/')
-    if request.method == 'POST':
-        print(request.form)
-        email = request.form['email'].strip().lower()
-        password = request.form['password'].strip()
-        query = """SELECT id, fname, password FROM users WHERE email = ?"""
-        con = create_connection(DATABASE)
-        cur = con.cursor()
-        cur.execute(query, (email,))
-        user_data = cur.fetchall()
-        con.close()
-        try:
-            userid = user_data[0][0]
-            firstname = user_data[0][1]
-            db_password = user_data[0][2]
-        except IndexError:
-            return redirect("/login?error=Email+invalid+or+password+incorrect")
-        if not bcrypt.check_password_hash(db_password, password):
-            return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
-        session['email'] = email
-        session['userid'] = userid
-        session['firstname'] = firstname
-        print(session)
-        return redirect('/')
-    return render_template("login.html", logged_in=is_logged_in(), categories=categories())
-
-@app.route('/logout')
-def logout():
-    print(list(session.keys()))
-    [session.pop(key) for key in list(session.keys())]
-    print(list(session.keys()))
-    return redirect('/?message=See+you+next+time!')
-
-@app.route('/add_category', methods=["GET", "POST"])
-def render_add_category_page():
-    if request.method == 'POST':
-        print(request.form)
-        cat_name = request.form['category_name'].strip().title()
-        query = "INSERT INTO categories(id, category_name) VALUES(NULL,?)"  # inserts into "categories".
-        con = create_connection(DATABASE)
-        cur = con.cursor()
-        try:
-            cur.execute(query, (cat_name, ))
-        except sqlite3.ProgrammingError:
-            return redirect('/?error=Incorrect+number+of+bindings+supplied')
-        con.commit()
-        con.close()
-        return redirect("/")
-    error = request.args.get('error')
-    if error == None:
-        error = ""
-    return render_template("add_category.html", error=error, logged_in=is_logged_in(), categories=categories())
-
+# User Signup
 @app.route('/signup', methods=['GET', 'POST'])
 def render_signup_page():
     if is_logged_in():
@@ -138,5 +93,87 @@ def render_signup_page():
         error = ""
     return render_template("signup.html", error=error)
 
+# User Login
+@app.route('/login', methods=["GET", "POST"])
+def render_login_page():
+    if is_logged_in():
+        return redirect('/')
+    if request.method == 'POST':
+        print(request.form)
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip()
+        query = """SELECT id, fname, password FROM users WHERE email = ?"""
+        con = create_connection(DATABASE)
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_data = cur.fetchall()
+        con.close()
+        try:
+            userid = user_data[0][0]
+            firstname = user_data[0][1]
+            db_password = user_data[0][2]
+        except IndexError:
+            return redirect("/login?error=Email+invalid+or+password+incorrect")
+        if not bcrypt.check_password_hash(db_password, password):
+            return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
+        session['email'] = email
+        session['userid'] = userid
+        session['firstname'] = firstname
+        print(session)
+        return redirect('/')
+    return render_template("login.html", logged_in=is_logged_in(), categories=categories())
+
+# User Logout
+@app.route('/logout')
+def logout():
+    print(list(session.keys()))
+    [session.pop(key) for key in list(session.keys())]
+    print(list(session.keys()))
+    return redirect('/?message=See+you+next+time!')
+
+# User can add a Catergory
+@app.route('/add_category', methods=["GET", "POST"])
+def render_add_category_page():
+    if request.method == 'POST':
+        print(request.form)
+        cat_name = request.form['category_name'].strip().title()
+        query = "INSERT INTO categories(id, category_name) VALUES(NULL,?)"  # inserts into "categories".
+        con = create_connection(DATABASE)
+        cur = con.cursor()
+        try:
+            cur.execute(query, (cat_name, ))
+        except sqlite3.ProgrammingError:
+            return redirect('/?error=Incorrect+number+of+bindings+supplied')
+        con.commit()
+        con.close()
+        return redirect("/")
+    error = request.args.get('error')
+    if error == None:
+        error = ""
+    return render_template("add_category.html", error=error, logged_in=is_logged_in(), categories=categories())
+
+# Displaying the Category page
+@app.route('/category/<Cat_id>')
+def render_category_page(Cat_id):
+    con = create_connection(databse)
+
+# Displaying the contents for the specified category
+    query = "SELECT id, Maori, English, Cat_id, Definition, Level, Image, date FROM dictionary"
+    cur = con.cursor()
+    cur.execute(query)
+    contents = cur.fetchall()
+
+    query = "SELECT id, category_name FROM categories"
+    cur = con.cursor()
+    cur.execute(query)
+    specific_category = cur.fetchall()
+    con.commit()
+    con.close()
+
+    return render_template('category.html', contents = contents,
+                           specific_category = specific_category, categories = categories(), Cat_id = int(Cat_id))
+
+
+# Running the app
 if __name__ == '__main__':
     app.run()
